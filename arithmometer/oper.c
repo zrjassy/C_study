84b8871358fe44a807d4fef5c92c3c6049941bb3
zrjassy <834398599@qq.com>
2020-10-14 14:42:14 +0800

    add 四则运算

---
+++ arithmometer/oper.c
@@ -0,0 +1,27 @@
+int sum(int a, int b)
+{
+    int s = 0;
+    s = a + b;
+    return s;
+}
+
+int sub(int a, int b)
+{
+    int s = 0;
+    s = a - b;
+    return s;
+}
+
+int mul(int a, int b)
+{
+    int s = 0;
+    s = a * b;
+    return s;
+}
+
+int div(int a, int b)
+{
+    int s = 0;
+    s = a / b;
+    return s;
+}
