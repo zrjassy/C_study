5e9f3f9c767e56d0c88455fbd83ceeabf649c36d
zrjassy <834398599@qq.com>
2021-01-19 14:34:36 +0800

    update

--- Insertionsort/main.c
+++ Insertionsort/main.c
@@ -38,6 +38,7 @@ struct max MaxSubSequenceSum1(const int A[], int N)
     return Max;
 }
 
+// 算法2
 struct max MaxSubSequenceSum2(const int A[], int N)
 {
     int thisSum, MaxSum, i, j;
@@ -65,15 +66,40 @@ struct max MaxSubSequenceSum2(const int A[], int N)
     return Max;
 }
 
+// 算法3
+struct max MaxSubSequenceSum3(const int A[], int N)
+{
+    int ThisSum, MaxSum, j, Begin, End;
+    Begin = End = 0;
+    ThisSum = MaxSum = 0;
+    for (j = 0; j < N; j++)
+    {
+        ThisSum += A[j];
+        if (ThisSum > MaxSum)
+            MaxSum = ThisSum;
+        else if (ThisSum < 0)
+            ThisSum = 0;
+    }
+    struct max Max;
+    Max.maxsum = MaxSum;
+    Max.begin = Begin;
+    Max.end = End;
+
+    return Max;
+};
+
 int main()
 {
     int A[7] = {1, -2, 3, 1, -2, 0, 1};
     int N = 7;
     struct max Max1 = MaxSubSequenceSum1(A, N);
     struct max Max2 = MaxSubSequenceSum2(A, N);
+    struct max Max3 = MaxSubSequenceSum3(A, N);
     printf("calcu1:\n");
     printf("maxsum:%d\nbegin:%d\nend:%d\n", Max1.maxsum, Max1.begin, Max1.end);
     printf("calcu2:\n");
     printf("maxsum:%d\nbegin:%d\nend:%d\n", Max2.maxsum, Max2.begin, Max2.end);
+    printf("calcu3:\n");
+    printf("maxsum:%d\nbegin:%d\nend:%d\n", Max3.maxsum, Max3.begin, Max3.end);
     return 0;
 }
