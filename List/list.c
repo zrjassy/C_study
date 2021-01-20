d480b8b06780d06c36625b92de86044d522a2ada
zrjassy <834398599@qq.com>
2021-01-20 15:25:37 +0800

    update

--- List/list.c
+++ List/list.c
@@ -10,27 +10,29 @@ typedef struct student
 LinkList *creat(int n)
 {
     LinkList *head, *node, *end;
-    head = (LinkList*)malloc(sizeof(LinkList));
-    end=head;
-    for(int i=0;i<n;i++){
-        node=(LinkList*)malloc(sizeof(LinkList));
-        scanf("%d",&node->score);
-        end->next=node;
-        end=node;
-    } 
-    end->next=NULL;
+    head = (LinkList *)malloc(sizeof(LinkList));
+    end = head;
+    for (int i = 0; i < n; i++)
+    {
+        node = (LinkList *)malloc(sizeof(LinkList));
+        scanf("%d", &node->score);
+        end->next = node;
+        end = node;
+    }
+    end->next = NULL;
     return head;
 }
 
-int main(){
+int main()
+{
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     LinkList *head = creat(n);
 
-    while(head->next !=NULL)
+    while (head->next != NULL)
     {
         head = head->next;
-        printf("%d\n",head->score);
+        printf("%d\n", head->score);
     }
     return 0;
 }
