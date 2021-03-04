ceb8e6925f64e0f8d31123f3a9e4c5448d985373
zrjassy <834398599@qq.com>
2021-03-04 17:26:36 +0800

    update linklist链表

--- LinkList/list.c
+++ LinkList/list.c
@@ -7,7 +7,7 @@ typedef struct student
     struct student *next;
 } LinkList;
 
-LinkList *creat(int n)
+LinkList *Creat(int n)
 {
     LinkList *head, *node, *end;
     head = (LinkList *)malloc(sizeof(LinkList));
@@ -23,16 +23,55 @@ LinkList *creat(int n)
     return head;
 }
 
-int main()
+// 按序号查找节点
+LinkList *Get(LinkList *head, int i)
 {
-    int n;
-    scanf("%d", &n);
-    LinkList *head = creat(n);
+    int j;
+    LinkList *p;
+    p = head;
+    j = 0;
+    while ((p->next != NULL) && (j < i))
+    {
+        p = p->next;
+        j++;
+    }
+    if (i == j)
+        return p;
+    else
+        return NULL;
+}
 
+// 按值查找节点的位置
+LinkList *Locate(LinkList *head, int score)
+{
+    LinkList *p;
+    p = head->next;
+    while ((p != NULL) && (p->score != score))
+        p = p->next;
+    if (p == NULL)
+        return NULL;
+    else
+        return p;
+}
+
+void Output(LinkList *head)
+{
     while (head->next != NULL)
     {
         head = head->next;
         printf("%d\n", head->score);
     }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    LinkList *head = Creat(n);
+    // Output(head);
+    LinkList *p1 = Get(head, 3);
+    printf("%d\n", p1->score);
+    LinkList *p2 = Locate(head, 15);
+    printf("%p\n", p2);
     return 0;
 }
