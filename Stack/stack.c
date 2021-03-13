f98b2f0e229cbea2221aab44318988cd18e94602
zrjassy <834398599@qq.com>
2021-03-13 16:16:09 +0800

    update stack

--- Stack/stack.c
+++ Stack/stack.c
@@ -3,9 +3,60 @@
 #define maxsize 1024
 typedef int datatype;
 
-
 typedef struct Stack
 {
     datatype elements[maxsize];
     int Top;
-} ;
+} stack;
+
+// 置空栈
+stack *SetNullS(stack *S)
+{
+    S->Top = -1;
+    return S;
+}
+
+// 判断栈是否为空
+int EmptyS(stack *S)
+{
+    if (S->Top >= 0)
+        return 0;
+    else
+        return 1;
+}
+
+// 进栈
+stack *PushStackS(stack *S, int e)
+{
+    if (S->Top >= maxsize - 1)
+    {
+        printf("Stack Overflow\n");
+        return NULL;
+    }
+    else
+    {
+        S->Top++;
+        S->elements[S->Top] = e;
+    }
+    return S;
+}
+
+// 出栈
+datatype *PopS(stack *S)
+{
+    datatype *ret;
+    if(EmptyS(S))
+    {
+        printf("Stack underflow\n");
+        return NULL;
+    }
+    else
+    {
+        S->Top--;
+        ret = (datatype *)malloc(sizeof(datatype));
+        *ret = S->elements[S->Top+1];
+        return ret;
+    }
+}
+
+
