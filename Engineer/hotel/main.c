ce8fd49f479112d176e2f9654e4dfd6050318b25
jassy <834398599@qq.com>
2021-05-11 14:56:20 +0800

    update hotel

---
+++ Engineer/hotel/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+int mainmenu()
+{
+    printf("\t\t\t      酒店管理系统\n");
+    printf("\t\t\t===============================\n");
+    printf("\t\t\t      1 : 管理\n");
+    printf("\t\t\t      2 : 住宿\n");
+    printf("\t\t\t      3 : 用餐\n");
+    printf("\t\t\t      0 : 退出\n");
+    printf("\t\t\t===============================\n");
+    printf("please select:");
+    int c;
+    scanf("%d", &c);
+    while (c < 0 || c > 2)
+    {
+        printf("input error,again:\n");
+        scanf("%d", &c);
+    }
+    return c;
+}
+
+int main(int argc, char const *argv[])
+{
+    int c;
+    int loop = 1;
+    while (loop)
+    {
+        system("cls");
+        c=mainmenu();
+        switch(c)
+		{
+			case 1://control
+				break;
+			case 2://living
+				break;
+            case 3://eating
+				break;
+			default:
+				loop = 0;
+				break;
+		}
+    }
+    return 0;
+}
